bae712687de2025be2b01f67160f1762dada41ba
Name <mail>
2023-03-29 23:10:17 +0500

    10.3 start, lucci done

--- map/[PinkieWork]AssembleMap.bat
+++ map/[PinkieWork]AssembleMap.bat
@@ -1,3 +1,3 @@
 call "C:\pjass.exe" "C:\common.j" "C:\Blizzard.j" war3map.j
-"C:\Users\Maxim\Downloads\w3x2lni_enUS_v2.7.1\w2l.exe" slk "C:\Choice\ChoiceBattleVadik\Choice-Battle" "C:\Choice\Choice_Battle_2_10_2.w3x"
+"C:\Users\Maxim\Downloads\w3x2lni_enUS_v2.7.1\w2l.exe" slk "C:\Choice\ChoiceBattleVadik\Choice-Battle" "C:\Choice\Choice_Battle_2_10_3.w3x"
 pause
