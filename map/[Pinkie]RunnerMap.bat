d879b33dd2c9b045bd54dc88ae960d8b5cd5d0de
Name <mail>
2023-04-01 18:32:17 +0500

    2.10.6 Tornados + Doflamingo + Onidzuka

--- map/[Pinkie]RunnerMap.bat
+++ map/[Pinkie]RunnerMap.bat
@@ -1,1 +1,1 @@
-"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_2_10_5.w3x"
+"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_2_10_6.w3x"
