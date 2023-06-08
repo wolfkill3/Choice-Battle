991bac17531918755d78cdb8e888f420c787e419
PinkieNecro <NecromanseR_RuS@mail.ru>
2023-06-08 16:04:55 +0500

    2.11.12 start

--- map/[Pinkie]RunnerMap.bat
+++ map/[Pinkie]RunnerMap.bat
@@ -1,1 +1,1 @@
-"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_2_11_11.w3x"
+"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_2_11_12.w3x"
