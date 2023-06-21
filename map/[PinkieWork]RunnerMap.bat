c706774914c0162e75ae5de795b2809b0ccfc160
PinkieNecro <NecromanseR_RuS@mail.ru>
2023-06-21 12:24:13 +0500

    KoreanTranslation part 1

--- map/[PinkieWork]RunnerMap.bat
+++ map/[PinkieWork]RunnerMap.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Warcraft 3 Frozen Throne\war3.exe" -loadfile "maps\download\Choice_Battle_2_11_15.w3x"
+"C:\Program Files (x86)\Warcraft 3 Frozen Throne\war3.exe" -loadfile "maps\download\Choice_Battle_2_11_15_KR.w3x"
--- map/[Pinkie]RunnerMap.bat
+++ map/[Pinkie]RunnerMap.bat
@@ -1,1 +1,1 @@
-"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_2_11_15.w3x"
+"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_2_11_15_KR.w3x"
