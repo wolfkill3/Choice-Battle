9547fc9baa2e95e689b8bc6552b8915c43480e5d
Name <mail>
2023-07-06 15:09:29 +0500

    MINUS MEMHACK + UJAPI + several bug fixes

--- map/[Pinkie]RunnerMap.bat
+++ map/[Pinkie]RunnerMap.bat
@@ -1,1 +1,1 @@
-"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_2_11_18.w3x"
+"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_2_12.w3x"
