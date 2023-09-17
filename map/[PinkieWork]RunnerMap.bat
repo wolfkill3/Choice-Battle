ad26a8bfd5e50ec1802e1744dc0528d3a4a31ccc
Name <mail>
2023-09-17 19:04:00 +0500

    3.0.11 start

--- map/[PinkieWork]RunnerMap.bat
+++ map/[PinkieWork]RunnerMap.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Warcraft 3 Frozen Throne\war3.exe" -loadfile "maps\download\Choice_Battle_3_0_10.w3x"
+"C:\Program Files (x86)\Warcraft 3 Frozen Throne\war3.exe" -loadfile "maps\download\Choice_Battle_3_0_11.w3x"
--- map/[Pinkie]RunnerMap.bat
+++ map/[Pinkie]RunnerMap.bat
@@ -1,1 +1,1 @@
-"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_3_0_10.w3x"
+"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_3_0_11.w3x"
