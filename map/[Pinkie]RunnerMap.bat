03d19278d4eff25d087d3a145077113251232b5e
Name <mail>
2023-10-22 23:58:34 +0500

    3.0.20 start

--- map/[Pinkie]RunnerMap.bat
+++ map/[Pinkie]RunnerMap.bat
@@ -1,1 +1,1 @@
-"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_3_0_19.w3x"
+"D:\Program Files (x86)\Warcraft III\war3.exe" -loadfile "maps\download\Choice_Battle_3_0_20.w3x"
