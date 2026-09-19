65dadb8bc9fcd8e4daebb4beac1e67753e16646a
PinkieNecro <NecromanseR_RuS@mail.ru>
2026-09-19 14:20:58 +0500

    3.7.2 start

--- map/[Pinkie]AssembleMap.bat
+++ map/[Pinkie]AssembleMap.bat
@@ -1,4 +1,4 @@
 call "C:\Users\Maksim\Downloads\pjass.exe" "C:\Users\Maksim\Downloads\common.j" "C:\Users\Maksim\Downloads\Blizzard.j" war3map.j
-"C:\Users\Maksim\Downloads\w3x2lni_enUS_v2.7.2\w2l.exe" slk "C:\Games\Choice\images\Choice-Battle" "C:\Program Files (x86)\Warcraft III\Maps\Download\Choice_Battle_3_7_1.w3x"
-"C:\Users\Maksim\Downloads\w3x2lni_enUS_v2.7.2\w2l.exe" slk "C:\Games\Choice\images\Choice-Battle" "C:\Games\Choice\Choice_Battle_3_7_1.w3x"
+"C:\Users\Maksim\Downloads\w3x2lni_enUS_v2.7.2\w2l.exe" slk "C:\Games\Choice\images\Choice-Battle" "C:\Program Files (x86)\Warcraft III\Maps\Download\Choice_Battle_3_7_2.w3x"
+"C:\Users\Maksim\Downloads\w3x2lni_enUS_v2.7.2\w2l.exe" slk "C:\Games\Choice\images\Choice-Battle" "C:\Games\Choice\Choice_Battle_3_7_2.w3x"
 pause
